8abda5b2f32eb2903683bb7e58405b51553cc3d3
Anna-Maria Sairanen <anna-maria.sairanen@student.careeria.fi>
2024-02-05 22:10:30 +0200

    buttonit laitettu kuntoon

--- server.js
+++ server.js
@@ -22,3 +22,26 @@ app.use(express.static(polku))
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
 })
+$(document).ready(function(){
+
+    })
+    $("#paasivu").click(function(){
+        $(".main_col").empty();
+        var txt1 = $("<p></p>").text("Teemme hiekkapuhalluksia, maalauksia ja kaikenlaisia autojen korjaustöitä");
+        var tx = $("<p></p>").text("Myymme jenkkiautojen purkuosia ja entisöimme vanhoja amerikanrautoja.");
+        $(".main_col").append(txt1, tx)
+
+    $("#esittely").click(function(){
+      $(".main_col").empty();
+      var txt1 = $("<p></p>").text("Teemme hiekkapuhalluksia, maalauksia ja kaikenlaisia autojen korjaustöitä");
+      var tx = $("<p></p>").text("Myymme jenkkiautojen purkuosia ja entisöimme vanhoja amerikanrautoja.");
+      $(".main_col").append(txt1, tx)
+      
+    })
+    $("#yhteystiedot").click(function(){
+      $(".content").empty();
+      var txt2 = $("<p></p>").text("Tommi Ryynänen p. 0400-1394459");
+      var txt3 = $("<p></p>").text("mail@rustbros.fi");
+$(".content").append(txt2, txt3)
+    })
+    })
